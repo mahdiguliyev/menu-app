e2ca41a2b170e0203f006f767ccde31674a1212d
Mahdi Guliyev <quliyev.mehdi.1997@gmail.com>
2023-04-30 19:58:38 +0400

    select unique categories from the data

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,18 @@ import { useState } from "react";
 import Title from "./Title";
 import menu from "./data";
 import Menu from "./Menu";
+
+/* const tempCategories = menu.map((item) => item.category);
+// get only unique categories
+const categories = new Set(tempCategories);
+const filters = ["all", categories]; */
+
+// one line code example of above
+const allCategories = ["all", ...new Set(menu.map((item) => item.category))];
+
 const App = () => {
   const [menuItems, setMenuItems] = useState(menu);
+  const [categories, setCategories] = useState(allCategories);
   return (
     <main>
       <section className="menu">
