0f0b42936b8cce172dd4bfe915ece14afae15c9a
Mahdi Guliyev <quliyev.mehdi.1997@gmail.com>
2023-04-30 19:52:00 +0400

    initial commit - listed menu

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import Title from "./Title";
+import menu from "./data";
+import Menu from "./Menu";
+const App = () => {
+  const [menuItems, setMenuItems] = useState(menu);
+  return (
+    <main>
+      <section className="menu">
+        <Title text="our menu" />
+        <Menu menuItems={menuItems} />
+      </section>
+    </main>
+  );
+};
+export default App;
